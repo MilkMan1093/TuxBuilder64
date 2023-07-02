087fca3a0c860a6a5d6df7a34e0cea14e656bc64
MilkMan1093 <88229702+MilkMan1093@users.noreply.github.com>
2023-07-02 12:28:06 +0300

    Update tuxbuildcli.py to ver0.2.0

--- tuxbuildcli.py
+++ tuxbuildcli.py
@@ -1,20 +1,23 @@
 import time
+import colorama
+from colorama import Fore, Back, Style
 import os
+colorama.init(autoreset=True)
 
-print("Welcome to the TuxBuilder CLI!")
+print(f"{Fore.RED}RED text means important information.")
+print(f"{Fore.GREEN}GREEN text means general comments.")
+print(f"{Fore.BLUE}BLUE text indicates user input.\n")
+time.sleep(2)
 
-time.sleep(1)
+print(f"{Fore.GREEN}TuxBuilderCLI: A tool to easily build the SM64 PC Port without fuss.")
 
-print("\n")
-print("This tool will help you build the Super Mario 64 PC Port on Linux without fuss!")
 
 def dependencies():
-    distropkgmgr = input('''Which package manager does your distribution use?
-    1. APT (Debian, Ubuntu , Zorin OS, elementary, Linux Mint, Pop!_OS and the other 1920839840938290 Ubuntu-based distros)
+    distropkgmgr = input(f'''{Fore.CYAN}Select your distribution's package manager-\n
+    1. APT (Debian, Ubuntu , Zorin OS, elementaryOS, Linux Mint, Pop!_OS and the other 1920839840938290 Ubuntu-based distros)
     2. Pacman (Arch, Manjaro, EndeavourOS, Garuda etc.)
     3. XBPS-install (Void Linux)
-    4. DNF (Fedora)
-    5. "      " (slackware) 
+    4. DNF (Fedora) 
     --->  ''')
     if distropkgmgr == "APT" or "apt" or "1":
         os.system("sudo apt install build-essential git python3 libglew-dev libsdl2-dev curl libcurl4-gnutls-dev libjsoncpp-dev")
@@ -25,7 +28,7 @@ def dependencies():
     elif distropkgmgr == "DNF" or "dnf":
         os.system("sudo dnf install make gcc python3 glew-devel SDL2-devel")
     elif distropkgmgr == "slackware" or "      ":
-        print(":[ sorry")
+        print(":( sorry")
     else:
         print("Invalid")
 
@@ -34,34 +37,33 @@ dependencies()
 def reposelect():
     global repo
     print("\n")
-    repo = input('''What repository will you be building today? \n
+    repo = input(f'''{Fore.BLUE}Select the repository you would like to build today. - \n
     You have a choice of:
     1. Render96ex (alpha, master, tester, tester_rt64alpha)
     2. SM64ex (nightly, master, coop, alo and more to come!)
     3. SM64rt (master, fsr)
-    4. Custom repository (not recommended) 
-    5. Saturn (best for Machinima-making purposes) (only legacy Saturn)
+    4. Saturn (best for Machinima-making purposes) (only legacy Saturn)
     So, what will it be? Choose the number stated before the repository name.    ''')
 
 reposelect()
 
 def render96sel():
-    confirmation = input("\nYou have selected Render96ex. After the confirmation, you will be asked to select which branch to use. Input yes or no BUT do not enter Y or N.  ")
-    if confirmation == "yes":
-        branchren96 = input('''Which branch to clone and build? \n
+    confirmation = input(f"\n{Fore.BLUE}You have selected Render96ex.\nIs the above selection correct? (y,n) ")
+    if confirmation == "y":
+        branchren96 = input(f'''\n{Fore.BLUE}Select the branch to be built. (Render96ex) \n
         1. Master (old, outdated)
         2. Alpha (best choice for regular players)
         3. Tester (bleeding edge features and bugfixes)
-        4. Tester_RT64Alpha (make sure you have a good enough GPU and drivers for this! As Linus Torvalds once said, "(expletive) you, NVIDIA"!) \n''')
+        4. Tester_RT64Alpha (Don't use if you run open-source drivers, like Nouveau.) \n''')
         if branchren96 == "1":
-            print("\nPLEASE put your Super Mario 64 ROM in your home folder and rename it to 'baserom.us.z64' without the quotes!\n")
+            print("\nCopy your baserom to your home folder and rename it to baserom.us.z64.\n")
             time.sleep(3)
             os.system("git clone https://github.com/Render96/Render96ex --branch master")
             print("Copying baserom....")
             os.system("cp /home/$USER/baserom.us.z64 /home/$USER/TuxBuilder64/Render96ex/baserom.us.z64")
-            print("Baserom copied!\n")
+            print("Baserom copied, ready for building.\n")
             time.sleep(2)
-            buildspeed1 = input('''Now, it comes to building the game. How fast do you want the game to build?
+            buildspeed1 = input(f'''\n{Fore.BLUE}The repository has been cloned and the baserom has been successfully copied. Select the speed by which to build the game.
             1. Slow (-j2)
             2, Normal (-j4)
             3. Faster than normal (-j6)
@@ -80,14 +82,14 @@ def render96sel():
                 os.chdir("Render96ex")
                 os.system("make -j8")
         elif branchren96 == "2":
-            print("\nPLEASE put your Super Mario 64 ROM in your home folder and rename it to 'baserom.us.z64' without the quotes!\n")
+            print("\nCopy your baserom to your home folder and rename it to baserom.us.z64.\n")
             time.sleep(3)
             os.system("git clone https://github.com/Render96/Render96ex --branch alpha")
             print("Copying baserom....")
             os.system("cp /home/$USER/baserom.us.z64 /home/$USER/TuxBuilder64/Render96ex/baserom.us.z64")
-            print("Baserom copied!\n")
+            print("Baserom copied.\n")
             time.sleep(2)
-            buildspeed1 = input('''Now, it comes to building the game. How fast do you want the game to build?
+            buildspeed1 = input(f'''{Fore.BLUE}The repository has been cloned and the baserom has been successfully copied. Select the speed by which to build the game.
             1. Slow (-j2)
             2, Normal (-j4)
             3. Faster than normal (-j6)
@@ -106,14 +108,14 @@ def render96sel():
                 os.chdir("Render96ex")
                 os.system("make -j8")
         elif branchren96 == "3":
-            print("\nPLEASE put your Super Mario 64 ROM in your home folder and rename it to 'baserom.us.z64' without the quotes!\n")
+            print("\nCopy your baserom to your home folder and rename it to baserom.us.z64.\n")
             time.sleep(3)
             os.system("git clone https://github.com/Render96/Render96ex --branch tester")
             print("Copying baserom....")
             os.system("cp /home/$USER/baserom.us.z64 /home/$USER/TuxBuilder64/Render96ex/baserom.us.z64")
-            print("Baserom copied!\n")
+            print("Baserom copied.\n")
             time.sleep(2)
-            buildspeed1 = input('''Now, it comes to building the game. How fast do you want the game to build?
+            buildspeed1 = input(f'''{Fore.BLUE}The repository has been cloned and the baserom has been successfully copied. Select the speed by which to build the game.
             1. Slow (-j2)
             2, Normal (-j4)
             3. Faster than normal (-j6)
@@ -132,18 +134,16 @@ def render96sel():
                 os.chdir("Render96ex")
                 os.system("make -j8")
         elif branchren96 == "4":
-            print("Put your Super Mario 64 ROM in your home folder and rename it to 'baserom.us.z64' without the quotes!\n")
-            time.sleep(3)
-            pleaseconfirm = input("WARNING! Are you sure that you want to clone the tester RT64 BRANCH? It won't be fun if you don't have a decent GPU! ")
-            if pleaseconfirm == "yes":
-                print("\nPLEASE put your Super Mario 64 ROM in your home folder and rename it to 'baserom.us.z64' without the quotes!")
+            pleaseconfirm = input(f"{Fore.RED}|WARNING| You have selected the RT64 branch. Is this correct?  ")
+            if pleaseconfirm == "y":
+                print("\nCopy your baserom to your home folder and rename it to baserom.us.z64.")
                 time.sleep(3)
                 os.system("git clone https://github.com/Render96/Render96ex --branch tester_rt64alpha")
                 print("Copying baserom....")
                 os.system("cp /home/$USER/baserom.us.z64 /home/$USER/TuxBuilder64/Render96ex/baserom.us.z64")
-                print("Baserom copied!\n")
+                print("Baserom copied.\n")
             time.sleep(2)
-            buildspeed1 = input('''Now, it comes to building the game. How fast do you want the game to build?
+            buildspeed1 = input(f'''{Fore.BLUE}The repository has been cloned and the baserom has been successfully copied. Select the speed by which to build the game.
             1. Slow (-j2)
             2, Normal (-j4)
             3. Faster than normal (-j6)
@@ -163,12 +163,12 @@ def render96sel():
                 os.system("make -j8")
             elif pleaseconfirm == "no":
                 render96sel()
-    elif confirmation == "no":
+    elif confirmation == "n":
         reposelect()
 
 def sm64exsel():
-    smexconf = input("\n You have selected SM64EX. You will be asked to select which branch to use after the confirmation. Input yes or no! (NOT y or n) ")
-    if smexconf == "yes":
+    smexconf = input("\nYou have selected SM64ex.\nIs the above selection correct (y,n) ")
+    if smexconf == "y":
         branchsmex = input('''Which branch to clone and build? \n
         1. Nightly (recommended)
         2. Master (alright, I guess)
@@ -176,14 +176,14 @@ def sm64exsel():
         4. Alo (Honestly don't know what the difference is, but I'm sure it's good!)
         5. There are WAYYY too many SM64ex branches. This is only an alpha build. More to come! ''')
         if branchsmex == "1":
-            print("\nPLEASE put your Super Mario 64 ROM in your home folder and rename it to 'baserom.us.z64' without the quotes!\n")
+            print("\nCopy your baserom to your home folder and rename it to baserom.us.z64.\n")
             time.sleep(3)
             os.system("git clone https://github.com/sm64pc/sm64ex --branch nightly")
             print("Copying baserom....")
             os.system("cp /home/$USER/baserom.us.z64 /home/$USER/TuxBuilder64/sm64ex/baserom.us.z64")
-            print("Baserom copied!\n")
+            print("Baserom copied.\n")
             time.sleep(2)
-            buildspeed1 = input('''Now, it comes to building the game. How fast do you want the game to build?
+            buildspeed1 = input('''The repository has been cloned and the baserom has been successfully copied. Select the speed by which to build the game.
             1. Slow (-j2)
             2, Normal (-j4)
             3. Faster than normal (-j6)
@@ -202,14 +202,14 @@ def sm64exsel():
                 os.chdir("sm64ex")
                 os.system("make -j8")
         elif branchsmex == "2":
-            print("\nPLEASE put your Super Mario 64 ROM in your home folder and rename it to 'baserom.us.z64' without the quotes!\n")
+            print("\nCopy your baserom to your home folder and rename it to baserom.us.z64.\n")
             time.sleep(3)
             os.system("git clone https://github.com/sm64pc/sm64ex --branch master")
             print("Copying baserom....")
             os.system("cp /home/$USER/baserom.us.z64 /home/$USER/TuxBuilder64/sm64ex/baserom.us.z64")
-            print("Baserom copied!")
+            print("Baserom copied.")
             time.sleep(2)
-            buildspeed1 = input('''Now, it comes to building the game. How fast do you want the game to build?
+            buildspeed1 = input('''The repository has been cloned and the baserom has been successfully copied. Select the speed by which to build the game.
             1. Slow (-j2)
             2, Normal (-j4)
             3. Faster than normal (-j6)
@@ -228,14 +228,14 @@ def sm64exsel():
                 os.chdir("sm64ex")
                 os.system("make -j8")
         elif branchsmex == "3":
-            print("\nPLEASE put your Super Mario 64 ROM in your home folder and rename it to 'baserom.us.z64' without the quotes!")
+            print("\nCopy your baserom to your home folder and rename it to baserom.us.z64.")
             time.sleep(3)
             os.system("git clone https://github.com/djoslin0/sm64ex-coop")
             print("Copying baserom....")
             os.system("cp /home/$USER/baserom.us.z64 /home/$USER/TuxBuilder64/sm64ex-coop/baserom.us.z64")
-            print("Baserom copied!\n")
+            print("Baserom copied.\n")
             time.sleep(2)
-            buildspeed1 = input('''Now, it comes to building the game. How fast do you want the game to build?
+            buildspeed1 = input('''The repository has been cloned and the baserom has been successfully copied. Select the speed by which to build the game.
             1. Slow (-j2)
             2, Normal (-j4)
             3. Faster than normal (-j6)
@@ -254,14 +254,14 @@ def sm64exsel():
                 os.chdir("sm64ex-coop")
                 os.system("make -j8")
         elif branchsmex == "4":
-            print("\nPLEASE put your Super Mario 64 ROM in your home folder and rename it to 'baserom.us.z64' without the quotes!\n")
+            print("\nCopy your baserom to your home folder and rename it to baserom.us.z64.\n")
             time.sleep(3)
             os.system("git clone https://github.com/AloXado320/sm64ex-alo")
             print("Copying baserom....")
             os.system("cp /home/$USER/baserom.us.z64 /home/$USER/TuxBuilder64/sm64ex-alo/baserom.us.z64")
-            print("Baserom copied!\n")
+            print("Baserom copied.\n")
             time.sleep(2)
-            buildspeed1 = input('''Now, it comes to building the game. How fast do you want the game to build?
+            buildspeed1 = input('''The repository has been cloned and the baserom has been successfully copied. Select the speed by which to build the game.
             1. Slow (-j2)
             2, Normal (-j4)
             3. Faster than normal (-j6)
@@ -279,24 +279,24 @@ def sm64exsel():
             elif buildspeed1 == "4":
                 os.chdir("sm64ex-alo")
                 os.system("make -j8")
-    elif smexconf == "no":
+    elif smexconf == "n":
         reposelect()
 
 def sm64rtsel():
     sm64rtconf = input("You have selected SM64RT. You will be asked to select which branch to clone after the confirmation. Input yes or no! (NOT y or n) ")
     if sm64rtconf == "yes":
         branchsm64rt = input('''You have selected SM64RT. Here are the two available branches:
-        1. Master (what you should normally use)
-        2. fsr (can anyone tell me the difference?)''')
+        1. Master
+        2. fsr''')
         if branchsm64rt == "1":
-            print("\nPLEASE put your Super Mario 64 ROM in your home folder and rename it to 'baserom.us.z64' without the quotes!\n")
+            print("\nCopy your baserom to your home folder and rename it to baserom.us.z64.\n")
             time.sleep(3)
             os.system("git clone https://github.com/DarioSamo/sm64rt --branch master")
             print("Copying baserom....")
             os.system("cp /home/$USER/baserom.us.z64 /home/$USER/TuxBuilder64/sm64rt/baserom.us.z64")
-            print("Baserom copied!\n")
+            print("Baserom copied.\n")
             time.sleep(2)
-            buildspeed1 = input('''Now, it comes to building the game. How fast do you want the game to build?
+            buildspeed1 = input('''The repository has been cloned and the baserom has been successfully copied. Select the speed by which to build the game.
             1. Slow (-j2)
             2, Normal (-j4)
             3. Faster than normal (-j6)
@@ -315,14 +315,14 @@ def sm64rtsel():
                 os.chdir("sm64rt")
                 os.system("make -j8 RENDER_API=RT64 EXTERNAL_DATA=1")
         elif branchsm64rt == "2":
-            print("\nPLEASE put your Super Mario 64 ROM in your home folder and rename it to 'baserom.us.z64' without the quotes!\n")
+            print("\nCopy your baserom to your home folder and rename it to baserom.us.z64.\n")
             time.sleep(3)
             os.system("git clone https://github.com/DarioSamo/sm64rt --branch fsr")
             print("Copying baserom....")
             os.system("cp /home/$USER/baserom.us.z64 /home/$USER/TuxBuilder64/sm64rt/baserom.us.z64")
-            print("Baserom copied!\n")
+            print("Baserom copied.\n")
             time.sleep(2)
-            buildspeed1 = input('''Now, it comes to building the game. How fast do you want the game to build?
+            buildspeed1 = input('''The repository has been cloned and the baserom has been successfully copied. Select the speed by which to build the game.
             1. Slow (-j2)
             2, Normal (-j4)
             3. Faster than normal (-j6)
@@ -341,30 +341,20 @@ def sm64rtsel():
                 os.chdir("sm64rt")
                 os.system("make -j8")
 
-def customrepouse():
-    whoopsiesmaybe = input("Are you sure you want to select a custom repository? You will have to manually copy the BaseROM to the folder. ")
-    if whoopsiesmaybe == "yes":
-        x = input("input the repo WITH  'git clone'! (e.g. git clone https://github.com/user-or-group/repository-name) ")
-        print(x)
-        time.sleep(1)
-        os.system(x)
-    elif whoopsiesmaybe == "no":
-        reposelect()
-
 def saturnsel():
-    saturnconfirm = input("You have selected Saturn. Nifty little repository for copying SMG4- just kidding! Are you sure you want to select this repository? Input yes or no, but NOT y or n. ")
+    saturnconfirm = input("You have selected Saturn. Nifty little repository for copying SMG4- just kidding! Are you sure you want to select this repository? Input yes or no, but NOT y or n. sS")
     if saturnconfirm == "yes":
-        print("\nPLEASE put your Super Mario 64 ROM in your home folder and rename it to 'baserom.us.z64' without the quotes!")
+        print("\nPut your Super Mario 64 baserom in your home folder andrename it to baserom.us.z64\n")
         time.sleep(3)
         os.system("git clone https://github.com/Llennpie/Saturn --branch legacy")
         print("Copying baserom....")
         os.system("cp /home/$USER/baserom.us.z64 /home/$USER/TuxBuilder64/Saturn/baserom.us.z64")
-        print("Baserom copied!")
+        print("Baserom copied.")
         print("You will be asked to enter your Sudo password in a second. This is to fix an issue with building on Linux, where make spits out a 'No such file or disrectory' error about the file 'json/json.h'.")
         time.sleep(1)
         os.system("sudo ln -s /usr/include/jsoncpp/json/ /usr/include/json")
         time.sleep(2)
-        buildspeed1 = input('''Now, it comes to building the game. How fast do you want the game to build?
+        buildspeed1 = input('''The repository has been cloned and the baserom has been successfully copied. Select the speed by which to build the game.
         1. Slow (-j2)
         2, Normal (-j4)
         3. Faster than normal (-j6)
@@ -393,10 +383,8 @@ elif repo == "2":
 elif repo == "3":
     sm64rtsel()
 elif repo == "4":
-    customrepouse()
-elif repo == "5":
     saturnsel()
 else:
-    print("That was invalid. Try again and restart the script. \n")
+    print("Invalid input. Try again and restart the script. \n")
 
-print("If you encounter any problems in building, try again, and follow the instructions properly.")
+print(f"{Fore.RED}If you encounter any problems in building, try again, and follow the instructions properly.")
